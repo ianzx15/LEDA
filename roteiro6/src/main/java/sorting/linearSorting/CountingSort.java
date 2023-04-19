ad5a94234762ed5256b881761fd66b71bbd1b834
ian <ian>
2023-04-19 09:45:17 -0300

    u

---
+++ roteiro6/src/main/java/sorting/linearSorting/CountingSort.java
@@ -0,0 +1,36 @@
+package sorting.linearSorting;
+
+import sorting.AbstractSorting;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Classe que implementa a estratégia de Counting Sort vista em sala.
+ *
+ * Procure evitar desperdício de memória: AO INVÉS de alocar o array de contadores
+ * com um tamanho arbitrariamente grande (por exemplo, com o maior valor de entrada possível),
+ * aloque este array com o tamanho sendo o máximo inteiro presente no array a ser ordenado.
+ *
+ * Seu algoritmo deve assumir que o array de entrada nao possui numeros negativos,
+ * ou seja, possui apenas numeros inteiros positivos e o zero.
+ *
+ */
+public class CountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		int max = Collections.max(Arrays.asList(array));
+		Integer[] frequencia = new Integer[max];
+		populaListaAuxiliar(array, frequencia);
+	}
+
+	public void populaListaAuxiliar(Integer[] array, Integer[] frequencia){
+		for (int j = 0; j <  array.length; j++){
+				frequencia[array[j] - 1] += 1;
+		}
+		System.out.println(Arrays.toString(frequencia));
+	}
+
+}
