401e755bf6cf89c40e7c57388d3f686fa3963b1b
ian.rodrigues@ <ian.rodrigues@>
2023-04-27 11:42:03 -0300

    a

--- roteiro7/src/main/java/problems/FloorBinarySearchImpl.java
+++ roteiro7/src/main/java/problems/FloorBinarySearchImpl.java
@@ -9,41 +9,33 @@ public class FloorBinarySearchImpl implements Floor {
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
 		sort(array, 0, array.length -1);
-		int b = binarySearch(array, x, 0 , array.length);
-
-		System.out.println(Arrays.toString(array));
-		System.out.println(b);
-		return null;
+		int b = binarySearch(array, x, 0 , array.length - 1);
+		return b;
 	}
 
 	private int binarySearch(Integer[] array, int num, int left, int right){
 		if (left > right) {
 			return -1;
 		}
-		int m = (right + left) / 2;
-		//maior que o maior da lista
-		if (num > array[m] && left == 1){
-			return m;
-		}
-		// maior que o segundo maior da lista e menor que o primeiro
-		if (left == 1 && num < array[m]){
-			return m - 1;
-		}
-		//menor que o menor numero da lista
-		if (m == 0 && num < array[m]){
-			return -1;
-		}
 
-		if (num == array[m]){
-			return m;
-		}
+		int m = (right + left)/ 2;
 
-		if(num < array[m]) {
-			return binarySearch(array, num, left, m - 1);
-		}
-		else{
-			return  binarySearch(array, num, m + 1, right);
+		if (left == right){
+			if (num >= array[m]){
+				return m;
+			}else {
+				return m - 1;
 			}
+		} else if(m == 0 && num < array[m]){
+			return  - 1;
+		} else if(num < array[m]){
+			return binarySearch(array, num, left, m);
+		} else if (num > array[m]){
+			return  binarySearch(array, num, m + 1, right);
+		} else {
+			return m;
+		}
+
 	}
 
 	private void sort(Integer[] array, int leftIndex, int rightIndex) {
@@ -67,19 +59,5 @@ public class FloorBinarySearchImpl implements Floor {
 		Util.swap(array, leftIndex, i);
 		return i;
 	}
-
-	public static void main(String[] args) {
-		Floor a = new FloorBinarySearchImpl();
-		Integer[] array = new Integer[5];
-		array[0] = 1;
-		array[1] = 10;
-		array[2] = 3;
-		array[3] = 4;
-		array[4] = 2;
-		a.floor(array,9 );
-
-	}
-
-
 
 }
