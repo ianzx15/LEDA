ea53c7a33bd957f37895388ba7249923ebfab928
ian <ian>
2023-05-15 23:38:18 -0300

    u

--- roteiro9/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
+++ roteiro9/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
@@ -25,13 +25,37 @@ public class RecursiveDoubleLinkedListImpl<T> extends
 	@Override
 	public void insertFirst(T element) {
 		if (element != null){
+			if (isEmpty()){
+				this.setNext(new RecursiveDoubleLinkedListImpl());
+				this.setData(element);
+				this.setPrevious(new RecursiveDoubleLinkedListImpl());
+			} else{
+				RecursiveDoubleLinkedListImpl newItem = new RecursiveDoubleLinkedListImpl();
+				newItem.setData(this.getData());
+				newItem.setNext(this.getNext());
+				this.setNext(newItem);
+				this.setData(element);
+				newItem.setPrevious(this);
+				((RecursiveDoubleLinkedListImpl) newItem.getNext()).setPrevious(newItem);
+			}
 		}
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+			if (!isEmpty()){
+				if (this.getNext().isEmpty()){
+					this.setPrevious(null);
+					this.setData(null);
+					this.setNext(null);
+				} else{
+					this.setData(this.getNext().getData());
+					this.setNext(this.getNext().getNext());
+					((RecursiveDoubleLinkedListImpl<T>) this.getNext()).setPrevious(this);
+				}
+
+			}
+
 	}
 
 	@Override
